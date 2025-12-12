2b762c3b6afef919962c54307052460ccfe7bae0
zsagia <zsagia@gmail.com>
2025-12-12 22:02:10 +0100

    Greeting component with signal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Greeting } from './greeting/greeting';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Greeting],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = 'odett-angular';
+  name = signal('Odett');
+  age = 15;
+
+  constructor() {
+    this.name.set('Welcome Odett');
+  }
 }
